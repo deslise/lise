76ec7535c04091c0bc13e8409b576cf1ea61ece5
Nayara Silva <nayara.silva.f@hotmail.com>
2017-09-26 18:40:53 -0300

    alterações no modelo

--- managedata/admin.py
+++ managedata/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from managedata.models import *
+
+admin.site.register(Branch)
+admin.site.register(CategoryBusiness)
+admin.site.register(EarlyBusinessPlan)
+admin.site.register(LateBusinessPlan)
+admin.site.register(Business)
+admin.site.register(Keyword)
+admin.site.register(Opinion)
+admin.site.register(Review)
+admin.site.register(Topic)
--- managedata/models.py
+++ managedata/models.py
@@ -3,15 +3,28 @@ from django.db import models
 
 # Create your models here.
 
+class Branch(models.Model):
+    name = models.CharField(max_length=240)
+
+    def __str__(self):
+        return self.name
+
 
 
 class CategoryBusiness(models.Model):
-    description = models.CharField(max_length=240)
+    specialty = models.CharField(max_length=240)
+    branch = models.ForeignKey('Branch', related_name='categories')
+
+    def __str__(self):
+        return self.specialty
 
 
 class Keyword(models.Model):
     keyword = models.CharField(max_length=50)
     category = models.ForeignKey('CategoryBusiness', on_delete=models.CASCADE, related_name='keywords')
+
+    def __str__(self):
+        return self.keyword
 
 
 class Business(models.Model):
@@ -19,11 +32,17 @@ class Business(models.Model):
     place_id = models.CharField(max_length=50)
     category = models.ForeignKey('CategoryBusiness', on_delete=models.CASCADE, related_name='related_business')
 
+    def __str__(self):
+        return self.name
+
 
 class Review(models.Model):
     author_id = models.CharField(max_length=50)
     complete_text = models.CharField(max_length=500)
     business = models.ForeignKey('Business', on_delete=models.CASCADE, related_name='reviews')
+
+    def __str__(self):
+        return self.complete_text
 
 
 class Opinion(models.Model):
@@ -32,11 +51,17 @@ class Opinion(models.Model):
     polality = models.IntegerField()
     review = models.ForeignKey('Review', on_delete=models.CASCADE, related_name='opinions')
 
+    def __str__(self):
+        return self.text_pt
 
-class topic(models.Model):
+
+class Topic(models.Model):
     topic = models.CharField(max_length=240)
     opinion = models.ForeignKey('Opinion', on_delete=models.CASCADE, related_name='topics')
     relations_topics = models.ManyToManyField('self')
+
+    def __str__(self):
+        return self.topic
 
 
 class BusinessPlan(models.Model):
@@ -45,13 +70,23 @@ class BusinessPlan(models.Model):
     location = models.CharField(max_length=100)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
+    class Meta:
+        abstract = True
+
 
 class LateBusinessPlan(BusinessPlan):
     business = models.OneToOneField('Business', on_delete=models.CASCADE, related_name='late_business_plan')
 
+    def __str__(self):
+        return self.description + ' (late)'
+
 
 class EarlyBusinessPlan(BusinessPlan):
     category = models.ForeignKey('CategoryBusiness', on_delete=models.CASCADE, related_name='early_business_plans')
+
+    def __str__(self):
+        return self.description + ' (early)'
+
 
 
 
